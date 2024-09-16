83e4394ed4245034c8fb42ce2740cbc6f1dbb0ab
sarahossam1234 <158767344+sarahossam1234@users.noreply.github.com>
2024-09-16 16:13:41 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿using Core.Framework.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace FirstProject
+{
+    public partial class Form1 : Form
+    {
+        private SqlDatabaseManager mgrSQL;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mgrSQL = new SqlDatabaseManager(".", "TESTDB", "sa", "password");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mgrSQL.ExecuteSelectQuery("Select * from testTable", new List<SqlParameter>());
+        }
+    }
+}
