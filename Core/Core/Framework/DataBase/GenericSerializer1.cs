3548c099091c36f3ce63c10eb2ef017d79a906b2
sarahossam1234 <158767344+sarahossam1234@users.noreply.github.com>
2024-09-27 05:04:38 +0300

    Update GenericSerializer1.cs

--- Core/Core/Framework/DataBase/GenericSerializer1.cs
+++ Core/Core/Framework/DataBase/GenericSerializer1.cs
@@ -1,1 +1,18 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
 
+namespace Core.Framework.DataBase
+{
+    internal class GenericSerializer
+    {
+        internal T ReadObjectData(string configFilePath) =>
+            
+            Deserialize(configFilePath);
+
+        private T Deserialize(string configFilePath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
