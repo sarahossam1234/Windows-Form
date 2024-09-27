0cb41bfbce008fa2dd85fec7ed11139e1bbf4e6b
sarahossam1234 <158767344+sarahossam1234@users.noreply.github.com>
2024-09-27 05:02:33 +0300

    Update DbConfig.cs

--- Core/Core/Framework/DataBase/DbConfig.cs
+++ Core/Core/Framework/DataBase/DbConfig.cs
@@ -1,1 +1,11 @@
+namespace Core.Framework.DataBase
+{
+    public class DbConfig
+    {
+        public string ServerName { get; set; }
+        public string DatabaseName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
 
+}
